f2b95c61674150dd1d5e5dfe5c65f01b2704fcd3
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2021-09-24 23:55:29 +0200

    chore: update HtmlWebpackPlugin template path with path.resolve

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
   ],
 };
