934503b120654bf8956fa14949a49469de6f11ba
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2021-09-25 18:49:23 +0200

    feat: game logic

--- src/board/Board.tsx
+++ src/board/Board.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { MouseEvent, useCallback, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import styled from "styled-components";
+
+type Mark = "X" | "O" | "";
 
 const Cell = styled(Col)`
   min-height: 200px;
   min-width: 200px;
+  font-size: 8rem;
+  text-align: center;
 `;
 
 // ----------------------------------
@@ -51,22 +55,35 @@ const Cell9 = styled(Cell)`
 `;
 
 function Board() {
+  const [cell1, setCell1] = useState<Mark>("");
+  const [cell2, setCell2] = useState<Mark>("");
+  const [cell3, setCell3] = useState<Mark>("");
+  const [cell4, setCell4] = useState<Mark>("");
+  const [cell5, setCell5] = useState<Mark>("");
+  const [cell6, setCell6] = useState<Mark>("");
+  const [cell7, setCell7] = useState<Mark>("");
+  const [cell8, setCell8] = useState<Mark>("");
+  const [cell9, setCell9] = useState<Mark>("");
+
+  // TODO rather choose player, not toggle sign
+  const getMark = (mark: Mark) => (mark === "X" ? "O" : "X");
+
   return (
     <Container className="h-100 d-flex flex-column justify-content-center">
       <Row>
-        <Cell1 />
-        <Cell2 />
-        <Cell3 />
+        <Cell1 onClick={() => setCell1(getMark)}>{cell1}</Cell1>
+        <Cell2 onClick={() => setCell2(getMark)}>{cell2}</Cell2>
+        <Cell3 onClick={() => setCell3(getMark)}>{cell3}</Cell3>
       </Row>
       <Row>
-        <Cell4 />
-        <Cell5 />
-        <Cell6 />
+        <Cell4 onClick={() => setCell4(getMark)}>{cell4}</Cell4>
+        <Cell5 onClick={() => setCell5(getMark)}>{cell5}</Cell5>
+        <Cell6 onClick={() => setCell6(getMark)}>{cell6}</Cell6>
       </Row>
       <Row>
-        <Cell7 />
-        <Cell8 />
-        <Cell9 />
+        <Cell7 onClick={() => setCell7(getMark)}>{cell7}</Cell7>
+        <Cell8 onClick={() => setCell8(getMark)}>{cell8}</Cell8>
+        <Cell9 onClick={() => setCell9(getMark)}>{cell9}</Cell9>
       </Row>
     </Container>
   );
