0a617d16a44ad37436c6554b3dc2c7199f25582f
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2021-09-26 19:50:09 +0200

    feat: win condition logic

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, useCallback, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import styled from "styled-components";
 
@@ -65,6 +65,69 @@ function Game() {
   const [cell8, setCell8] = useState<Mark>("");
   const [cell9, setCell9] = useState<Mark>("");
   const [mark, setMark] = useState<Mark>("O");
+  const winCondition =
+    /**
+     * XXX OOO
+     * ... ...
+     * ... ...
+     */
+    (cell1 === "X" && cell2 === "X" && cell3 === "X") ||
+    (cell1 === "O" && cell2 === "O" && cell3 === "O") ||
+    /**
+     * ... ...
+     * XXX OOO
+     * ... ...
+     */
+    (cell4 === "X" && cell5 === "X" && cell6 === "X") ||
+    (cell4 === "O" && cell5 === "O" && cell6 === "O") ||
+    /**
+     * ... ...
+     * ... ...
+     * XXX OOO
+     */
+    (cell7 === "X" && cell8 === "X" && cell9 === "X") ||
+    (cell7 === "O" && cell8 === "O" && cell9 === "O") ||
+    /**
+     * X.. O..
+     * X.. O..
+     * X.. O..
+     */
+    (cell1 === "X" && cell4 === "X" && cell7 === "X") ||
+    (cell1 === "O" && cell4 === "O" && cell7 === "O") ||
+    /**
+     * .X. .O.
+     * .X. .O.
+     * .X. .O.
+     */
+    (cell2 === "X" && cell5 === "X" && cell8 === "X") ||
+    (cell2 === "O" && cell5 === "O" && cell8 === "O") ||
+    /**
+     * ..X ..O
+     * ..X ..O
+     * ..X ..O
+     */
+    (cell3 === "X" && cell6 === "X" && cell9 === "X") ||
+    (cell3 === "O" && cell6 === "O" && cell9 === "O") ||
+    /**
+     * X.. O..
+     * .X. .O.
+     * ..X ..O
+     */
+    (cell1 === "X" && cell5 === "X" && cell9 === "X") ||
+    (cell1 === "O" && cell5 === "O" && cell9 === "O") ||
+    /**
+     * ..X ..O
+     * .X. .O.
+     * X.. O..
+     */
+    (cell3 === "X" && cell5 === "X" && cell7 === "X") ||
+    (cell3 === "O" && cell5 === "O" && cell7 === "O");
+
+  useEffect(() => {
+    if (winCondition) {
+      window.alert(JSON.stringify(mark));
+    }
+  }, [winCondition]);
 
   const getMark = () => (mark === "X" ? "O" : "X");
 
